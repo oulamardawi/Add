63c2c8afc2ea8d931fe477514a010dd6434026c6
OulaMardawi <ola.mardawi@gmail.com>
2020-02-06 23:41:22 +0200

    first commit

---
+++ oula/src/oula/mine.java
@@ -0,0 +1,21 @@
+package oula;
+
+import java.util.Scanner;
+
+public class mine {
+
+	public static void main(String[] args) {
+		System.out.println("Enter First Number");
+		Scanner input = new Scanner(System. in);
+		int x= input. nextInt();
+		System.out.println("You entered " + x);
+		
+		System.out.println("Enter Second Number");
+		Scanner input2 = new Scanner(System. in);
+		int y = input2. nextInt();
+		System.out.println("You entered " + y);
+		
+
+		int z=x+y;
+		System.out.println("The Sum is = "+z);} }
+
